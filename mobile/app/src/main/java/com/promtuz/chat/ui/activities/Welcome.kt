8fd92e6f51161ad2aa66e870b7da89f61f0ee65f
iambhuv <notbhuvnesh@gmail.com>
2025-10-30 23:00:46 +0530

    plans for safely backing up the secret key

--- mobile/app/src/main/java/com/promtuz/chat/ui/activities/Welcome.kt
+++ mobile/app/src/main/java/com/promtuz/chat/ui/activities/Welcome.kt
@@ -9,6 +9,22 @@ import com.promtuz.chat.ui.screens.WelcomeScreen
 import com.promtuz.chat.ui.theme.PromtuzTheme
 import org.koin.android.ext.android.inject
 
+
+/**
+ * TODO:
+ *  - Current WelcomeScreen is very bland and unattractive, so gotta fix that as well,
+ *  - Gotta improve the UX as well.
+ *  - When generating the key pair, must ask user for proper way of storing / backing-up
+ *    the secret key via a dialog box *i suppose*
+ *  - Password must be hashed using very high computation power demanding Argon2id config,
+ *    doing such prevents brute-forcing the decryption of private key at huge extent
+ *  - The exact config of hashing will not be standardized for app,
+ *    but rather be chosen via circumstances of course backed by limits for minimum
+ *  - Must add support for automatically storing the backup on certain cloud storage services like Google Drive
+ *
+ *  User must provide a "secure" password for encrypting the secret key.
+ *  Hence, this encrypted blob is somewhat safe to move around *i suppose?*.
+ */
 class Welcome : ComponentActivity() {
     private lateinit var keyManager: KeyManager
 
