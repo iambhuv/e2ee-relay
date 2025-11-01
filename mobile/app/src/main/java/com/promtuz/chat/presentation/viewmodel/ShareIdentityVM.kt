2d71a011460c1c91b0f23faef3d5b6294446e5d9
iambhuv <notbhuvnesh@gmail.com>
2025-11-01 20:50:01 +0530

    Sharing Identity viewModel

--- mobile/app/src/main/java/com/promtuz/chat/presentation/viewmodel/ShareIdentityVM.kt
+++ mobile/app/src/main/java/com/promtuz/chat/presentation/viewmodel/ShareIdentityVM.kt
@@ -1,7 +1,15 @@
 package com.promtuz.chat.presentation.viewmodel
 
+import android.app.Application
+import android.content.Context
 import androidx.lifecycle.ViewModel
+import com.promtuz.chat.data.repository.UserRepository
 
-class ShareIdentityVM : ViewModel() {
+class ShareIdentityVM(
+    private val application: Application,
+    private val userRepository: UserRepository
+) : ViewModel() {
+    private val context: Context get() = application.applicationContext
+
 
 }
