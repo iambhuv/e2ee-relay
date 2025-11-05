af04cc663b92bdf2b5ae57b8d9ce2b7d81d4434d
iambhuv <notbhuvnesh@gmail.com>
2025-11-05 13:21:36 +0530

    NAVIGANIMATIONS

--- mobile/app/src/main/java/com/promtuz/chat/navigation/AppNavigation.kt
+++ mobile/app/src/main/java/com/promtuz/chat/navigation/AppNavigation.kt
@@ -1,39 +1,28 @@
 package com.promtuz.chat.navigation
 
-import androidx.activity.compose.LocalActivity
 import androidx.compose.animation.SizeTransform
-import androidx.compose.animation.core.CubicBezierEasing
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideOutHorizontally
-import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.*
-import androidx.compose.ui.window.*
 import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.runtime.rememberSaveableStateHolderNavEntryDecorator
-import androidx.navigation3.scene.DialogSceneStrategy
 import androidx.navigation3.ui.NavDisplay
 import com.promtuz.chat.presentation.viewmodel.AppVM
+import com.promtuz.chat.ui.constants.Naviganimation
+import com.promtuz.chat.ui.screens.AboutScreen
 import com.promtuz.chat.ui.screens.ChatScreen
 import com.promtuz.chat.ui.screens.HomeScreen
 import com.promtuz.chat.ui.screens.SavedUsersScreen
+import com.promtuz.chat.ui.screens.SettingsScreen
 
 
-@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun AppNavigation(
     appViewModel: AppVM
 ) {
-    val activity = LocalActivity.current
-    val navigator = appViewModel.navigator
     val backStack = appViewModel.backStack
-    val motionScheme = MaterialTheme.motionScheme
 
     NavDisplay(
         backStack,
@@ -46,24 +35,13 @@ fun AppNavigation(
         ),
         entryProvider = entryProvider {
             entry<Routes.App> { HomeScreen(appViewModel) }
-            entry<Routes.Chat> { key -> ChatScreen(key.userId) }
-            entry<Routes.SavedUsers>(
-                metadata = DialogSceneStrategy.dialog(
-                    DialogProperties(windowTitle = "Saved Users")
-                )
-            ) { SavedUsersScreen() }
+            entry<Routes.Chat> { key -> ChatScreen() }
+            entry<Routes.SavedUsers> { SavedUsersScreen() }
+            entry<Routes.Settings> { SettingsScreen() }
+            entry<Routes.About> { AboutScreen() }
         },
         sizeTransform = SizeTransform(clip = false),
-        transitionSpec = {
-            fadeIn(tween(350, easing = CubicBezierEasing(0.0f, 0.8f, 0.2f, 1.0f))) +
-                    slideInHorizontally(tween(500, easing = CubicBezierEasing(0.0f, 0.8f, 0.2f, 1.0f))) { it / 4 } togetherWith
-                    fadeOut(tween(300, easing = CubicBezierEasing(0.0f, 0.8f, 0.2f, 0.8f)))
-        },
-        popTransitionSpec = {
-            fadeIn(tween(300, easing = CubicBezierEasing(0.0f, 0.8f, 0.2f, 0.8f))) togetherWith
-                    fadeOut(tween(350, easing = CubicBezierEasing(0.8f, 0.25f, 0.25f, 1.0f))) +
-                    slideOutHorizontally(tween(350, easing = CubicBezierEasing(0.5f, 0.12f, 0.12f, 0.9f))) { (it * 0.75f).toInt() }
-        }
-
+        transitionSpec = { Naviganimation.transitionSpec() },
+        popTransitionSpec = { Naviganimation.popTransitionSpec() }
     )
 }
