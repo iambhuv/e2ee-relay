d72ed8bf3116fe24e337d2e875b6f46f0a545c81
iambhuv <notbhuvnesh@gmail.com>
2025-11-05 11:41:35 +0530

    added FlexibleScreen for general screens like Settings etc

--- mobile/app/src/main/java/com/promtuz/chat/ui/screens/SavedUsersScreen.kt
+++ mobile/app/src/main/java/com/promtuz/chat/ui/screens/SavedUsersScreen.kt
@@ -1,6 +1,5 @@
 package com.promtuz.chat.ui.screens
 
-import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -13,27 +12,21 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.*
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.*
 import androidx.compose.ui.input.nestedscroll.*
-import androidx.compose.ui.res.*
 import androidx.compose.ui.unit.*
-import com.promtuz.chat.R
+import com.promtuz.chat.data.local.entities.User
 import com.promtuz.chat.presentation.viewmodel.SavedUsersVM
 import com.promtuz.chat.ui.components.Avatar
+import com.promtuz.chat.ui.components.FlexibleScreen
 import com.promtuz.chat.ui.util.groupedRoundShape
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun SavedUsersScreen(viewModel: SavedUsersVM = koinViewModel()) {
-    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
     val isLoading by viewModel.isLoading.collectAsState()
 
-    val colors = MaterialTheme.colorScheme
-    val textTheme = MaterialTheme.typography
-
-    Scaffold(
-        Modifier.fillMaxSize(), topBar = { TopBar(scrollBehavior) }) { padding ->
+    FlexibleScreen({ Text("Saved Users") }) { padding, scrollBehavior ->
         if (isLoading) {
             Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
                 LoadingIndicator(Modifier.fillMaxSize(0.25f))
@@ -53,40 +46,7 @@ fun SavedUsersScreen(viewModel: SavedUsersVM = koinViewModel()) {
                         Text(groupTitle)
                     }
                     itemsIndexed(users) { index, user ->
-                        val interactionSource = remember { MutableInteractionSource() }
-
-                        Spacer(Modifier.height(4.dp))
-
-                        Row(
-                            Modifier
-                                .fillMaxWidth()
-                                .clip(groupedRoundShape(index, users.size))
-                                .background(colors.surfaceContainer.copy(0.75f))
-                                .combinedClickable(
-                                    interactionSource = interactionSource,
-                                    indication = ripple(color = colors.surfaceContainerHighest),
-                                    onClick = {},
-                                    onLongClick = {})
-                                .padding(vertical = 8.dp, horizontal = 10.dp),
-                            horizontalArrangement = Arrangement.spacedBy(12.dp),
-                            verticalAlignment = Alignment.CenterVertically) {
-
-                            Avatar(user.nickname, size = 38.dp)
-
-                            Column(verticalArrangement = Arrangement.spacedBy(2.dp)) {
-                                Row(
-                                    Modifier.fillMaxWidth(),
-                                    horizontalArrangement = Arrangement.SpaceBetween,
-                                    verticalAlignment = Alignment.Top
-                                ) {
-                                    Text(
-                                        user.nickname,
-                                        style = textTheme.titleMediumEmphasized,
-                                        color = colors.onSecondaryContainer
-                                    )
-                                }
-                            }
-                        }
+                        SavedUsersGroup(Modifier, index, user, users.size)
                     }
                 }
             }
@@ -94,29 +54,42 @@ fun SavedUsersScreen(viewModel: SavedUsersVM = koinViewModel()) {
     }
 }
 
-
 @Composable
-private fun TopBar(scrollBehavior: TopAppBarScrollBehavior) {
-    val backHandler = LocalOnBackPressedDispatcherOwner.current
-    val colors = MaterialTheme.colorScheme
+private fun SavedUsersGroup(modifier: Modifier, index: Int, user: User, size: Int) {
+    val interactionSource = remember { MutableInteractionSource() }
 
-    MediumFlexibleTopAppBar(
-        title = {
-        Text("Saved Users")
-    },
-        navigationIcon = {
-            IconButton(onClick = {
-                backHandler?.onBackPressedDispatcher?.onBackPressed()
-            }) {
-                Icon(
-                    painter = painterResource(R.drawable.i_back),
-                    "Go Back",
-                    Modifier.size(28.dp),
-                    MaterialTheme.colorScheme.onSurface
+    val colors = MaterialTheme.colorScheme
+    val textTheme = MaterialTheme.typography
+
+    Row(
+        modifier
+            .fillMaxWidth()
+            .clip(groupedRoundShape(index, size))
+            .background(colors.surfaceContainer.copy(0.75f))
+            .combinedClickable(
+                interactionSource = interactionSource,
+                indication = ripple(color = colors.surfaceContainerHighest),
+                onClick = {},
+                onLongClick = {}
+            )
+            .padding(vertical = 8.dp, horizontal = 10.dp),
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Avatar(user.nickname, size = 38.dp)
+
+        Column(verticalArrangement = Arrangement.spacedBy(2.dp)) {
+            Row(
+                Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.Top
+            ) {
+                Text(
+                    user.nickname,
+                    style = textTheme.titleMediumEmphasized,
+                    color = colors.onSecondaryContainer
                 )
             }
-        },
-        colors = TopAppBarDefaults.topAppBarColors(containerColor = colors.background),
-        scrollBehavior = scrollBehavior
-    )
+        }
+    }
 }
